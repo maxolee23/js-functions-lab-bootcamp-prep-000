b4bc5ded49a0db7da5a8aaf66e39156c4645498f
Max Lee <maxolee23@gmail.com>
2019-06-05 15:34:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function happyHolidays () {
   return('Happy holidays!');
 }
-function happyHolidaysTo () {
+function happyHolidaysTo (name) {
   return(`Happy holidays, ${name}!`);
 }
 function happyHolidayTo (holiday, person) {
