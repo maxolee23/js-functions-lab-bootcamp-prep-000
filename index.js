358f019d0f615190a920b366e58fc38412bc035e
Max Lee <maxolee23@gmail.com>
2019-06-05 15:30:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ function happyHolidays () {
 function happyHolidaysTo () {
   return(`Happy holidays, ${name}!`);
 }
+function happyHolidayTo (holiday, name) {
+  return(`Happy ${holiday}, ${name}!`);
+}
 function holidayCountdown (holiday, days) {
   return(`It's ${days} days until ${holiday}!`);
 }
